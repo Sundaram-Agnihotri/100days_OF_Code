dfd5e1d07671382df1c11d0e530ef7291d727b33
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2023-07-22 15:27:51 +0530

    Create Remove duplicates from an unsorted linked list.java

---
+++ Remove duplicates from an unsorted linked list.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) 
+    {
+         // Your code here
+         HashSet<Integer> hs = new HashSet<>();
+         
+        Node curr = head;
+        Node pre = null;
+         
+        while (curr!=null){
+            int data = curr.data;
+            
+            if (hs.contains(data))
+            pre.next = curr.next;
+            else {
+                hs.add(data);
+                pre = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
