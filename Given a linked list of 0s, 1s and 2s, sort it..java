792a993c0906bf14c8e300a921939b35c51e7eb3
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2023-07-23 14:56:36 +0530

    Create Given a linked list of 0s, 1s and 2s, sort it..java

---
+++ Given a linked list of 0s, 1s and 2s, sort it..java
@@ -0,0 +1,35 @@
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        int zero=0;
+        int one = 0;
+        int two = 0;
+        Node curr = head;
+        while (curr!= null){
+            if (curr.data ==0)
+            zero++;
+            else if (curr.data==1)
+            one++;
+            else if(curr.data==2)
+            two++;
+            curr = curr.next;
+        }
+        curr = head;
+        while (curr!=null){
+            if(zero!=0){
+                curr.data=0;
+                zero--;
+            }
+            else if(one!=0){
+                curr.data=1;
+                one--;
+            }
+            else if(two!=0){
+                curr.data= 2;
+            }
+            curr= curr.next;
+        }
+        return head;
+    }
