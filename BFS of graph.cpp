0c10954dee9958f179aabdc610ffc97f0162d6a2
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-07-31 19:24:43 +0530

    Create BFS of graph.cpp

---
+++ BFS of graph.cpp
@@ -0,0 +1,23 @@
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+                vector<int>bfs;
+        int vis[V] = {0};
+        vis[0] = 1;
+        
+        queue<int>q;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
