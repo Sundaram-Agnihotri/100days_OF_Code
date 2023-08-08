b06c445d1f884894d4c9a4bbd592a41c36a19c94
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-08-08 12:51:20 +0530

    Sundaram Committed

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,11 @@
+    int search(vector<int>& nums, int target) {
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
