3da6959dc21ec26e731fd507f33e8c8a335175d6
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-08-11 08:33:55 +0530

    Sundaram Comiitted

---
+++ 518. Coin Change II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int change(int sum, vector<int>& coins) {
+        int N = coins.size();
+       long long int dp[sum+1][N+1];
+       for(int i=0;i<sum+1;i++)
+       {
+           dp[i][0]=0;
+       }
+       for(int i=0;i<N+1;i++)
+       {
+           dp[0][i]=1;
+       }
+       for(int i=1;i<=sum;i++)
+       {
+           for(int j=1;j<=N;j++)
+           {
+               dp[i][j]=dp[i][j-1];
+               if(i-coins[j-1]>=0)
+               {
+                   dp[i][j]+=dp[i-coins[j-1]][j];
+               }
+           }
+       }
+       return dp[sum][N];
+    }
+};
