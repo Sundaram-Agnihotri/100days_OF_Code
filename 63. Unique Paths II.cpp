52055db5941c981b59b8c2d345e8f696d8ef3cd9
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-08-12 08:27:08 +0530

    Sundaram Committed

---
+++ 63. Unique Paths II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+ int n, m;
+    vector<vector<long long>> dp;
+    long long numsPaths(int i, int j, vector<vector<int>>& obstacleGrid)
+    {       
+        if (dp[i][j]!=-1) return dp[i][j];
+        if (obstacleGrid[i][j]==1) return dp[i][j]=0;
+        long long ans=0;
+        if (i>0) ans+=numsPaths(i-1, j, obstacleGrid);   //up
+        if (j>0) ans+=numsPaths(i, j-1, obstacleGrid);  //left
+        if (i==0 && j==0) ans=(obstacleGrid[0][0]==0)?1:0;   //top dowm approach
+        return dp[i][j]=ans;     
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        n=obstacleGrid.size();
+        m=obstacleGrid[0].size();
+        dp.assign(n, vector<long long>(m, -1));
+
+        return numsPaths(n-1, m-1, obstacleGrid);
+    }
+};
