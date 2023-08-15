b0708a32196c34d0c6fcbe24bb4e3d2997ea2e2b
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2023-08-15 23:47:22 +0530

    Kapil Narwani

---
+++ Partition List.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode list1 = new ListNode();
+        ListNode list2 = new ListNode();
+        ListNode l1 = list1;
+        ListNode l2 = list2;
+
+        while (head != null) {
+            if (head.val < x){
+                l1.next = head;
+                l1= l1.next;
+            }
+            else {
+                l2.next = head;
+                l2= l2.next;
+            }
+             head = head.next;
+        }
+         l1.next = list2.next;
+        l2.next =null;
+
+        return list1.next;
+    }
+}
