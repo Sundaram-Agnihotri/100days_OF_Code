31404f9dc0c6dab388b15179ad5185e7f379d1cc
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-08-28 07:58:03 +0530

    Sundaram Committed

---
+++ Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,21 @@
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+  // your code goes here
+ if(head == NULL)
+    return NULL;
+    
+    Node* curr = head;
+    while(curr!=NULL && curr->next!=NULL)
+    {
+        if(curr->data==curr->next->data)
+        {
+            curr->next = curr->next->next;
+        }
+            else
+            {
+                curr = curr->next;
+            }
+    }
+    return head;
+}
