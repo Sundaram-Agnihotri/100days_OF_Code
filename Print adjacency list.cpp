bbff661b853ad6226f4437174ecdb385c5335851
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-09-05 23:51:16 +0530

    Sundaram Committed

---
+++ Print adjacency list.cpp
@@ -0,0 +1,9 @@
+        vector<vector<int>> ans(V);
+        for(int i = 0; i < edges.size(); i++){
+            int a=edges[i].first;
+            int b=edges[i].second;
+            ans[a].push_back(b);
+            ans[b].push_back(a);
+        }
+        return ans;
+    }
