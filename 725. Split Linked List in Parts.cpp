0229d2ad7eb8b59dc9b4be7388cd29d07619ec1a
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-09-06 18:50:52 +0530

    Sundaram Committed

---
+++ 725. Split Linked List in Parts.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*>ans;
+        int len=0;
+        ListNode*temp=head;
+        while(temp)
+        {
+            len++;
+            temp=temp->next;
+        }
+        int size=len/k;
+        int rem=len%k;
+        temp=head;
+        for(int i=0;i<k;i++)
+        {
+            ListNode*end=temp;
+            if(rem>0)
+            {
+                int c=size;
+                while(c--)
+                {
+                    end=end->next;
+                }
+                ListNode*x=end->next;
+                end->next=NULL;
+                ans.push_back(temp);
+                temp=x;
+                rem--;
+            }
+            else
+            {
+                if(size==0)
+                {
+                    ans.push_back(NULL);    
+                }
+                else
+                {
+                    int c=size-1;
+                    while(c--)
+                    {
+                    end=end->next;
+                    }
+                    ListNode*x=end->next;
+                    end->next=NULL;
+                    ans.push_back(temp);
+                    temp=x;
+                }
+            }
+        }
+        return ans;
+    }
+};
