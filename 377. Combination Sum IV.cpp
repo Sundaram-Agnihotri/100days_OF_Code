3c3b1897dd9cc486e76b75713ca72263cdf82526
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-09-09 23:11:19 +0530

    Create 377. Combination Sum IV.cpp

---
+++ 377. Combination Sum IV.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        if(target < 0)
+            return 0;
+        else if(target == 0)
+            return 1;
+        
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        
+        for(int i = 1; i <= target; i++) {
+            long long ans = 0;
+            for(int j = 0; j < nums.size(); j++) {
+                if(i - nums[j] >= 0)
+                    ans += dp[i - nums[j]];
+            }
+            dp[i] = ans;
+        }
+
+        return dp[target];
+    }
+};
