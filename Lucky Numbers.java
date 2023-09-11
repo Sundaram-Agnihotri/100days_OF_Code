4468acdbe62940250dd2947c7946272bb0105585
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2023-09-11 23:49:53 +0530

    Kapil Naewani

---
+++ Lucky Numbers.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    // Complete the function
+    // n: Input n
+    // Return True if the given number is a lucky number else return False
+    public static boolean isLucky(int n)
+    {
+        for (int i =2; i<=n; i++){
+            if (n%i==0)
+            return false;
+            if(i>n)
+            return true;
+            n = n-(n/i);
+        }
+        return true;
+    }
+}
