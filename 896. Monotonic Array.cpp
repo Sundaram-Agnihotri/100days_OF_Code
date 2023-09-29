5f78e5d3fb9e60e427cf0d5fc8757f7732573e4a
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-09-29 07:28:18 +0530

    Create 896. Monotonic Array.cpp

---
+++ 896. Monotonic Array.cpp
@@ -0,0 +1,20 @@
+  bool isIncreasing(vector<int> &nums) {
+        for (int i=0; i<nums.size()-1; i++) {
+            if (nums[i] > nums[i+1]) return false;
+        }
+
+        return true;
+    }
+
+    bool isDecreasing(vector<int> &nums) {
+        for (int i=0; i<nums.size()-1; i++) {
+            if (nums[i] < nums[i+1]) return false;
+        }
+
+        return true;
+    }
+
+
+    bool isMonotonic(vector<int>& nums) {
+        return (isIncreasing(nums) || isDecreasing(nums));
+    }
